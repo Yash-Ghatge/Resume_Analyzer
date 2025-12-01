cb261ac0460ff55cc018f3c6b53a2eacf92544b9
Yash-Ghatge <yashghatge012@gmail.com>
2025-12-01 16:59:55 +0530

    update

--- Backend/db.js
+++ Backend/db.js
@@ -4,6 +4,9 @@ const ConnectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URL)
         console.log('Database connected')
+
+
+        
     } catch (error) {
         console.log('Database disconnecteds')
     }
